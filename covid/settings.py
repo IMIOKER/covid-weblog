31e7b9a71ab510aa52c817cf942be680392c38d0
Iman <85828680+IMIOKER@users.noreply.github.com>
2021-11-10 11:26:35 +0330

    heroku settings

--- covid/settings.py
+++ covid/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-5@l^y-54ho69fp6!tqn=p-)zd3t7x1)l2o6b_-7ulx+zl4e_ur
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['covidweblog.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -107,6 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
